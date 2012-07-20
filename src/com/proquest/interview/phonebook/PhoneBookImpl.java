409eeb1bc91d97e89d3765de72fa6139dd529d52
Vamsi <vganji@mail.primemediaanalysis.com>
2012-07-19 20:06:50 -0400

    Modified these to according to requirements

--- src/com/proquest/interview/phonebook/PhoneBookImpl.java
+++ src/com/proquest/interview/phonebook/PhoneBookImpl.java
@@ -3,29 +3,97 @@ package com.proquest.interview.phonebook;
 import java.util.List;
 
 import com.proquest.interview.util.DatabaseUtil;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PhoneBookImpl implements PhoneBook {
-	public List people;
-	
-	@Override
-	public void addPerson(Person newPerson) {
-		//TODO: write this method
-	}
-	
-	@Override
-	public Person findPerson() {
-		//TODO: write this method
-	}
-	
-	public static void main(String[] args) {
-		DatabaseUtil.initDB();  //You should not remove this line, it creates the in-memory database
 
-		/* TODO: create person objects and put them in the PhoneBook and database
-		 * John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
-		 * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
-		*/ 
-		// TODO: print the phone book out to System.out
-		// TODO: find Cynthia Smith and print out just her entry
-		// TODO: insert the new person objects into the database
-	}
+    public List people;
+
+    @Override
+    public void addPerson(Person newPerson) {
+        try {
+            DatabaseUtil.addPerson(newPerson);
+            System.out.println("Added Successfully");
+        } catch (SQLException ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
+    }
+
+    @Override
+    public Person findPerson(String firstName, String lastName) {
+        try {
+            return DatabaseUtil.findPerson(firstName, lastName);
+        } catch (SQLException ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+            return null;
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+            return null;
+        }
+    }
+
+    // I added a lookup method in DatabaseUtil to get a list of all Persons
+    // Also the findPeople might not return accurate result as there might be multiple people with the same name
+    // and we are not returning any array
+    
+    
+    
+    public static void main(String[] args) {
+        DatabaseUtil.initDB();  //You should not remove this line, it creates the in-memory database
+
+        /*
+         * TODO: create person objects and put them in the PhoneBook and
+         * database John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
+         * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
+         */
+        // TODO: print the phone book out to System.out
+        // TODO: find Cynthia Smith and print out just her entry
+        // TODO: insert the new person objects into the database
+
+        PhoneBookImpl pbi = new PhoneBookImpl();
+        Person p1 = new Person();
+        Person p2 = new Person();;
+
+        p1.name = "John Smith";
+        p1.phoneNumber = "(248) 123-4567";
+        p1.address = "1234 Sand Hill Dr, Royal Oak, MI";
+
+        p2.name = "Cynthia Smith";
+        p2.phoneNumber = "(824) 128-8758";
+        p2.address = "875 Main St, Ann Arbor, MI";
+
+        pbi.addPerson(p1);
+        pbi.addPerson(p2);
+
+        // Will list all the people in the phone book
+        try {
+            List people = DatabaseUtil.lookupPerson();
+
+            for (int i = 0; i < people.size(); i++) {
+
+                Person temp = (Person) people.get(i);
+                System.out.println("Name:" + temp.name);
+                System.out.println("Phone Number:" + temp.phoneNumber);
+                System.out.println("Address:" + temp.address);
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ERROR:" + ex.getMessage());
+        }
+
+        // Search for only particular people in phone book
+
+        Person p = pbi.findPerson("Cynthia", "Smith");
+
+        System.out.println("Name:" + p.name);
+        System.out.println("Phone Number:" + p.phoneNumber);
+        System.out.println("Address:" + p.address);
+
+    }
 }
--- src/com/proquest/interview/util/DatabaseUtil.java
+++ src/com/proquest/interview/util/DatabaseUtil.java
@@ -1,31 +1,83 @@
 package com.proquest.interview.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.proquest.interview.phonebook.Person;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * This class is just a utility class, you should not have to change anything here
+ * This class is just a utility class, you should not have to change anything
+ * here
+ *
  * @author rconklin
  */
 public class DatabaseUtil {
-	public static void initDB() {
-		try {
-			Connection cn = getConnection();
-			Statement stmt = cn.createStatement();
-			stmt.execute("CREATE TABLE PHONEBOOK (NAME varchar(255), PHONENUMBER varchar(255), ADDRESS varchar(255))");
-			stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Chris Johnson','(321) 231-7876', '452 Freeman Drive, Algonac, MI')");
-			stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Dave Williams','(231) 502-1236', '285 Huron St, Port Austin, MI')");
-			cn.commit();
-			cn.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		
-	}
-	public static Connection getConnection() throws SQLException, ClassNotFoundException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		return DriverManager.getConnection("jdbc:hsqldb:mem", "sa", "");
-	}
+
+    public static void initDB() {
+        try {
+            Connection cn = getConnection();
+            Statement stmt = cn.createStatement();
+            stmt.execute("CREATE TABLE PHONEBOOK (NAME varchar(255), PHONENUMBER varchar(255), ADDRESS varchar(255))");
+            stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Chris Johnson','(321) 231-7876', '452 Freeman Drive, Algonac, MI')");
+            stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('Dave Williams','(231) 502-1236', '285 Huron St, Port Austin, MI')");
+            cn.commit();
+            cn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.hsqldb.jdbcDriver");
+        return DriverManager.getConnection("jdbc:hsqldb:mem", "sa", "");
+    }
+
+    public static void addPerson(Person p) throws SQLException, ClassNotFoundException {
+        
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            stmt.execute("INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('" + p.name + "','" + p.phoneNumber + "', '" + p.address + "')");
+       
+    }
+    
+     public static Person findPerson(String firstname , String lastname ) throws SQLException, ClassNotFoundException {
+        
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * from PHONEBOOK where NAME='"+firstname+" "+lastname+"'");
+            Person p = new Person();
+            
+            while ( rs.next() ){
+                p.address = rs.getString("ADDRESS");
+                p.name = rs.getString("NAME");
+                p.phoneNumber = rs.getString("PHONENUMBER");
+                
+            }
+            
+            return p;
+    }
+     
+     public static List<Person> lookupPerson( ) throws SQLException, ClassNotFoundException {
+        
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * from PHONEBOOK");
+            List<Person> p = new ArrayList<Person>();
+            
+            while ( rs.next() ){
+                
+                Person temp = new Person();
+                temp.address = rs.getString("ADDRESS");
+                temp.name = rs.getString("NAME");
+                temp.phoneNumber = rs.getString("PHONENUMBER");
+                p.add(temp);
+            }
+            
+            return p;
+    }
+     
+     
 }
